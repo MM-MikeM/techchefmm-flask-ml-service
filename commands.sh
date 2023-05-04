557481d77cdcc96f4501c36eddd3af880b7f41c5
TechChefMM <mmendoza@mail.com>
2023-05-04 03:31:48 +0000

    edit commands.sh file - add comments.  202305032331. MM

--- commands.sh
+++ commands.sh
@@ -1,7 +1,18 @@
+# Create Python Virtual Environment
 python3 -m venv ~/.techchefmm-flask-ml-service
 source ~/.techchefmm-flask-ml-service/bin/activate
+
+# Initiate project scaffolding, run make install
 make install
+
+# Deploy web app
 az webapp up -n techchefmm-flask-ml-service
+
+# Test application, run locustfile.py file
 locust -f locustfile.py --headless -t 2s
+
+# Run prediction
 ./make_predict_azure_app.sh
+
+# Deactivate Python Virtual Environment
 deactivate
